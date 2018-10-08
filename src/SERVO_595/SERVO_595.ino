afa832caee978394b4f5f17559994d39cc10e313
williamfalinski <william_halflife@hotmail.com>
2018-10-08 19:17:02 -0300

    S1

--- src/SERVO_595/SERVO_595.ino
+++ src/SERVO_595/SERVO_595.ino
@@ -15,20 +15,11 @@
 #define SER       10//Pino Dados
 #define SRCLK     12//Pino Clock de comunicacao
 #define RCLK      11//Pino Latch
-//#define OE      11//Pino Enable saidas -> GND
+#define POT       A5//Potenciometro
+//#define OE      XX//Pino Enable saidas -> GND
 
 uint16_t DUTY_CYCLES[8] = {0,0,0,0,0,0,0,0}; //Periodo em HIGH de cada servo 
-uint8_t  DATA_SERVOS[8] = {0,1,0,0,0,1,0,0}; //Vetor de Binários para enviar ao 74595
-
-void setup(){
-  pinMode(SER, OUTPUT);
-  pinMode(SRCLK, OUTPUT);
-  pinMode(RCLK, OUTPUT);
-  //pinMode(POT, INPUT);
-  initialize_servos();
-  Timer1.initialize(PWM_PERIOD); // Inicializa o Timer1 e configura para um período de 20 milisegundos
-  Timer1.attachInterrupt(servoPulse); // Configura a função callback() como a função para ser chamada a cada interrupção do Timer1
-}
+uint8_t  DATA_SERVOS[8] = {0,0,0,0,0,0,0,0}; //Vetor de Binários para enviar ao 74595
 
 /*INICIALIZA SERVOS EM 0º*/
 /*Especificar numero de servos para evitar calcular servos inexistentes*/
@@ -39,18 +30,24 @@ void initialize_servos(){
 }
 
 void servoPulse(){
-  uint64_t PWM_Start = micros();
-  uint64_t PWM_Now = micros() - PWM_Start;
+  unsigned long PWM_Start = micros();
+  unsigned long PWM_Now = micros() - PWM_Start;
   /*Enquanto nao completar um pulso*/
-  while(PWM_Now < PWM_PERIOD){
+  do{
     setBits(PWM_Now);
-    loadData_74595();
-    PWM_Now = micros() - PWM_Start;
+    loadData_74595(); 
+    delayMicroseconds(1);
+    PWM_Now = (micros() - PWM_Start);  
   }
+  while(PWM_Now <= MAX_DUTY_CYCLE);
+  
+  /*Zera o resto do periodo*/
+  setBits(PWM_PERIOD);
+  loadData_74595(); 
 }
 
 /*Vetor de Bits para enviar ao 74595*/
-void setBits(uint64_t pwm_now){
+void setBits(unsigned long pwm_now){
   for(int i = 0; i < N_SERVOS; i++){
     if(pwm_now < DUTY_CYCLES[i]){
        DATA_SERVOS[i] = 1;
@@ -63,22 +60,34 @@ void setBits(uint64_t pwm_now){
 
 /*GRAVA INFORMAÇÕES NO 74595*/
 void loadData_74595(){
-  /*CLOCK DO LATCH*/
-  digitalWrite(RCLK, LOW);
   /*GRAVA DO ULTIMO PARA O PRIMEIRO BIT*/
+  /*CLOCK DO LATCH*/ 
+  digitalWrite(RCLK, LOW);
   for(int i=N_SERVOS-1; i>=0; i--){
     /*CLOCK DOS REGISTRADORES PRIMARIOS*/
     digitalWrite(SRCLK, LOW);
     digitalWrite(SER, DATA_SERVOS[i]);
     digitalWrite(SRCLK, HIGH);
-    /*FIM CLOCK DOS REGISTRADORES PRIMARIOS*/   
-  } 
+    /*FIM CLOCK DOS REGISTRADORES PRIMARIOS*/
+  }
   digitalWrite(RCLK, HIGH); 
   /*FIM CLOCK DO LATCH*/
 }
 
+void setup(){
+  Serial.begin(9600);
+  pinMode(SER, OUTPUT);
+  pinMode(SRCLK, OUTPUT);
+  pinMode(RCLK, OUTPUT);
+  pinMode(POT, INPUT);
+  //initialize_servos();
+  Timer1.initialize(PWM_PERIOD); // Inicializa o Timer1 e configura para um período de 20 milisegundos
+  Timer1.attachInterrupt(servoPulse); // Configura a função callback() como a função para ser chamada a cada interrupção do Timer1
+}
 
 void loop()
 {
-  //delay(500);
+  DUTY_CYCLES[1] = map(analogRead(POT), 0 ,1023, MIN_DUTY_CYCLE, MAX_DUTY_CYCLE);
+  Serial.println(DUTY_CYCLES[1]);
+  delay(0);
 }
